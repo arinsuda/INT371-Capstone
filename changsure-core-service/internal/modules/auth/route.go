311fe81339db7600379a76f739cc4d9b617f8ea1
arinsuda <28irin62@gmail.com>
2025-11-23 22:33:38 +0700

    fix router

--- changsure-core-service/internal/modules/auth/route.go
+++ changsure-core-service/internal/modules/auth/route.go
@@ -3,8 +3,7 @@ package auth
 import "github.com/gofiber/fiber/v3"
 
 func (h *Handler) RegisterRoutes(api fiber.Router) {
-	g := api.Group("/auth")
-	g.Post("/register", h.Register)
-	g.Post("/login", h.Login)
-	g.Post("/refresh", h.Refresh)
+	api.Post("/register", h.Register)
+	api.Post("/login", h.Login)
+	api.Post("/refresh", h.Refresh)
 }
