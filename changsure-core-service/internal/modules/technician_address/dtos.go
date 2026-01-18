a9c3d8d7448a88c52e6de3ba0610967d91f4ec7c
arinsuda <28irin62@gmail.com>
2026-01-18 11:24:31 +0700

    add response label

--- changsure-core-service/internal/modules/technician_address/dtos.go
+++ changsure-core-service/internal/modules/technician_address/dtos.go
@@ -50,27 +50,27 @@ type NearbyTechnicianRequest struct {
 type TechnicianAddressResponse struct {
 	ID uint `json:"id"`
 
-	Label       *string `json:"label,omitempty"`
-	PhoneNumber *string `json:"phone_number,omitempty"`
+	Label       string `json:"label"`
+	PhoneNumber string `json:"phone_number"`
 
-	HouseNumber *string `json:"house_number"`
-	Village     *string `json:"village"`
-	Moo         *string `json:"moo"`
-	Soi         *string `json:"soi"`
-	Road        *string `json:"road"`
+	HouseNumber string `json:"house_number"`
+	Village     string `json:"village"`
+	Moo         string `json:"moo"`
+	Soi         string `json:"soi"`
+	Road        string `json:"road"`
 
 	SubDistrictID uint `json:"sub_district_id"`
 	DistrictID    uint `json:"district_id"`
 	ProvinceID    uint `json:"province_id"`
 
-	SubDistrictName string `json:"sub_district_name,omitempty"`
-	DistrictName    string `json:"district_name,omitempty"`
-	ProvinceName    string `json:"province_name,omitempty"`
-	PostalCode      string `json:"postal_code,omitempty"`
+	SubDistrictName string `json:"sub_district_name"`
+	DistrictName    string `json:"district_name"`
+	ProvinceName    string `json:"province_name"`
+	PostalCode      string `json:"postal_code"`
 
-	Latitude  *float64 `json:"latitude"`
-	Longitude *float64 `json:"longitude"`
-	IsPrimary bool     `json:"is_primary"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	IsPrimary bool    `json:"is_primary"`
 
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
@@ -82,19 +82,55 @@ func ToResponse(a *TechnicianAddress, phone *string) TechnicianAddressResponse {
 	}
 
 	resp := TechnicianAddressResponse{
-		ID:          a.ID,
-		Label:       a.Label,
-		PhoneNumber: phone,
-		HouseNumber: a.HouseNumber,
-		Village:     a.Village,
-		Moo:         a.Moo,
-		Soi:         a.Soi,
-		Road:        a.Road,
-		Latitude:    a.Latitude,
-		Longitude:   a.Longitude,
-		IsPrimary:   a.IsPrimary,
-		CreatedAt:   a.CreatedAt.Format(time.RFC3339),
-		UpdatedAt:   a.UpdatedAt.Format(time.RFC3339),
+		ID: a.ID,
+
+		Label:       "",
+		PhoneNumber: "",
+
+		HouseNumber: "",
+		Village:     "",
+		Moo:         "",
+		Soi:         "",
+		Road:        "",
+
+		SubDistrictID: 0,
+		DistrictID:    0,
+		ProvinceID:    0,
+
+		SubDistrictName: "",
+		DistrictName:    "",
+		ProvinceName:    "",
+		PostalCode:      "",
+
+		Latitude:  0,
+		Longitude: 0,
+		IsPrimary: a.IsPrimary,
+
+		CreatedAt: a.CreatedAt.Format(time.RFC3339),
+		UpdatedAt: a.UpdatedAt.Format(time.RFC3339),
+	}
+
+	if a.Label != nil {
+		resp.Label = *a.Label
+	}
+	if phone != nil {
+		resp.PhoneNumber = *phone
+	}
+
+	if a.HouseNumber != nil {
+		resp.HouseNumber = *a.HouseNumber
+	}
+	if a.Village != nil {
+		resp.Village = *a.Village
+	}
+	if a.Moo != nil {
+		resp.Moo = *a.Moo
+	}
+	if a.Soi != nil {
+		resp.Soi = *a.Soi
+	}
+	if a.Road != nil {
+		resp.Road = *a.Road
 	}
 
 	if a.SubDistrictID != nil {
@@ -105,6 +141,13 @@ func ToResponse(a *TechnicianAddress, phone *string) TechnicianAddressResponse {
 	}
 	if a.ProvinceID != nil {
 		resp.ProvinceID = *a.ProvinceID
+	}
+
+	if a.Latitude != nil {
+		resp.Latitude = *a.Latitude
+	}
+	if a.Longitude != nil {
+		resp.Longitude = *a.Longitude
 	}
 
 	if a.SubDistrict != nil {
