d5bd11cb7110c8740fdf618d707882591f896ea0
arinsuda <28irin62@gmail.com>
2026-02-16 21:22:03 +0700

    fix matching

--- changsure-core-service/internal/modules/technician_matching/utils.go
+++ changsure-core-service/internal/modules/technician_matching/utils.go
@@ -2,25 +2,44 @@ package technicianmatching
 
 import (
 	technician "changsure-core-service/internal/modules/technician"
+	ts "changsure-core-service/internal/modules/technician_service"
 	"math"
 )
 
-func ExtractPriceRange(t technician.Technician) (float64, float64) {
+func ExtractPriceRange(t technician.Technician, targetServiceID *uint) (float64, float64) {
+
 	if len(t.Services) == 0 {
 		return 0, 0
 	}
-	svc := t.Services[0]
+
+	var svc *ts.TechnicianService
+
+	if targetServiceID != nil {
+		for i := range t.Services {
+			if t.Services[i].Service.ID == *targetServiceID {
+				svc = &t.Services[i]
+				break
+			}
+		}
+	}
+
+	// fallback → first service
+	if svc == nil {
+		svc = &t.Services[0]
+	}
 
 	switch svc.PricingType {
 	case "FIXED":
 		if svc.PriceFixed != nil {
 			return *svc.PriceFixed, *svc.PriceFixed
 		}
+
 	case "RANGE":
 		if svc.PriceMin != nil && svc.PriceMax != nil {
 			return *svc.PriceMin, *svc.PriceMax
 		}
 	}
+
 	return 0, 0
 }
 
@@ -31,44 +50,59 @@ func ExtractRating(t technician.Technician) float64 {
 	return 0.0
 }
 
-func SelectBestTechnician(list []technician.Technician, priority string) *technician.Technician {
+func SelectBestTechnician(
+	list []technician.Technician,
+	priority string,
+	serviceID *uint,
+) *technician.Technician {
+
 	if len(list) == 0 {
 		return nil
 	}
 
-	best := list[0]
+	bestIndex := 0
 
-	for _, t := range list {
+	for i := 1; i < len(list); i++ {
+
+		current := list[i]
+		best := list[bestIndex]
+
 		switch priority {
+
 		case "price":
-			tMin, _ := ExtractPriceRange(t)
-			bestMin, _ := ExtractPriceRange(best)
-			if tMin < bestMin {
-				best = t
+
+			curMin, _ := ExtractPriceRange(current, serviceID)
+			bestMin, _ := ExtractPriceRange(best, serviceID)
+
+			if curMin < bestMin {
+				bestIndex = i
 			}
 
 		case "rating":
-			if ExtractRating(t) > ExtractRating(best) {
-				best = t
+
+			if ExtractRating(current) > ExtractRating(best) {
+				bestIndex = i
 			}
 
 		case "balanced":
-			tMin, _ := ExtractPriceRange(t)
-			tScore := ExtractRating(t) / math.Log(tMin+2)
 
-			bMin, _ := ExtractPriceRange(best)
-			bScore := ExtractRating(best) / math.Log(bMin+2)
+			curMin, _ := ExtractPriceRange(current, serviceID)
+			bestMin, _ := ExtractPriceRange(best, serviceID)
 
-			if tScore > bScore {
-				best = t
+			curScore := ExtractRating(current) / math.Log(curMin+2)
+			bestScore := ExtractRating(best) / math.Log(bestMin+2)
+
+			if curScore > bestScore {
+				bestIndex = i
 			}
 
 		default:
-			if ExtractRating(t) > ExtractRating(best) {
-				best = t
+
+			if ExtractRating(current) > ExtractRating(best) {
+				bestIndex = i
 			}
 		}
 	}
 
-	return &best
+	return &list[bestIndex]
 }
