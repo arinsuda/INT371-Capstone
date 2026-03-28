7ece107af6dc0c425a17efcde15362b89cda83e4
Arin Sudakijjathorn <sixarin.thorn@gmail.com>
2026-03-29 05:00:03 +0700

    fix next config

--- changsure-admin/next.config.ts
+++ changsure-admin/next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-
-  assetPrefix: "/capstone25/cp25ssa1", 
-
+  basePath: "/capstone25/cp25ssa1",
+  assetPrefix: "/capstone25/cp25ssa1",
   images: {
     domains: ["i.pravatar.cc", "picsum.photos"],
   },
