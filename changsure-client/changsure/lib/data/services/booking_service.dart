c6687e982c2a316605e78589a04899aeaa2704b3
arinsuda <sixarin.thorn@gmail.com>
2026-03-29 21:24:01 +0700

    fix Booking Action Status

--- changsure-client/changsure/lib/data/services/booking_service.dart
+++ changsure-client/changsure/lib/data/services/booking_service.dart
@@ -12,13 +12,13 @@ extension BookingActionExtension on BookingAction {
   String get value {
     switch (this) {
       case BookingAction.accept:
-        return "ACCEPT";
+        return "ACCEPTED";
       case BookingAction.reject:
-        return "REJECT";
+        return "REJECTED";
       case BookingAction.start:
-        return "START";
+        return "IN_PROGRESS";
       case BookingAction.complete:
-        return "COMPLETE";
+        return "COMPLETED";
     }
   }
 }
